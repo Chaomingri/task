3e48a5615ad469bd4587d874717acedc87e7e59c
chaoming <3055589866@qq.com>
2022-07-28 17:55:18 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+import VueRouter from "vue-router";
+
+import router from "./router";
+
+Vue.config.productionTip = false;
+
+
+Vue.use(ElementUI);
+Vue.use(VueRouter);
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+  router:router
+}).$mount('#app');

1e87e20344bc651234869b3101fe86f1379cfaa3
chaoming <3055589866@qq.com>
2022-07-28 18:36:41 +0800

    修复路由依赖版本

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,7 @@ import VueRouter from "vue-router";
 
 export default new VueRouter({
     routes: [
-        {path: '*', redirect: '/404', hidden: true}
-    // {
-    //     path: "",
-    //     component:
-    // }
+        {path: '*', redirect: '/404', hidden: true},
+        {path:"/404",component:()=>import("@/views/404"),hidden: true}
 ]
 })

788e2725533ba8678f8a480e1bf8aea8a60b6b48
chaoming <3055589866@qq.com>
2022-07-29 10:16:11 +0800

    添加主页结构

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,9 @@ import VueRouter from "vue-router";
 
 export default new VueRouter({
     routes: [
-        {path: '*', redirect: '/404', hidden: true},
-        {path:"/login",component:()=>import("@/views/login/index")},
-        {path:"/404",component:()=>import("@/views/404"),hidden: true}
+        {path:"/login",component:()=>import("@/views/login/index"),hidden: true},
+        {path:"/404",component:()=>import("@/views/404"),hidden: true},
+        {path:"",component:()=>import("@/views/layout/LayOut")},
+        {path: '*', redirect: '/404', hidden: true}
 ]
 })
